04f60d3e8e47a2c8b252b4f93f01e1319dcac69c
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2022-01-13 17:03:03 -0600

    Clean up and delete comments

--- Music_Library_BE/musiclibrary/music/models.py
+++ Music_Library_BE/musiclibrary/music/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Song(models.Model):
     title = models.CharField(max_length=50)
     artist = models.CharField(max_length=50)
--- Music_Library_BE/musiclibrary/music/views.py
+++ Music_Library_BE/musiclibrary/music/views.py
@@ -5,7 +5,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status 
 
-# Create your views here.
 class SongList(APIView):
     
     def get(self, request):
