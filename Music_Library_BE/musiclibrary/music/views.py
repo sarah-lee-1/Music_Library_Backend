01712615fbbd5e29ac99f934cc6107664455c9bc
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2022-01-13 14:59:11 -0600

    Complete functions in Views.py

--- Music_Library_BE/musiclibrary/music/views.py
+++ Music_Library_BE/musiclibrary/music/views.py
@@ -12,11 +12,35 @@ class SongList(APIView):
         song = Song.objects.all()
         serializer = SongSerializer(song, many=True)
         return Response(serializer.data)
-    
+           
     def post(self, request):
         serializer = SongSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
+
+class SongDetail(APIView):  
+    def get_object(self, pk):
+        try:
+            return Song.objects.get(pk=pk)
+        except Song.DoesNotExist:
+            raise HTTP404
+     
+    def get(self, request, pk):
+        song = self.get_object(pk)
+        serializer = SongSerializer(song)
+        return Response(serializer.data)    
+
+    def put(self, request, pk):     #update one song 
+        song = self.get_object(pk)
+        serializer = SongSerializer(song, data=request.data)
+        if serializer.is.valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        song = self.get_object(pk)
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
