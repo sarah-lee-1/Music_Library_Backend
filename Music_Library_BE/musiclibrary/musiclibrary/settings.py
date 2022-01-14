2488541fe7fff095777e6203f1b3afb8fc506308
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2022-01-14 09:37:33 -0600

    Add Corsheader to settings.py

--- Music_Library_BE/musiclibrary/musiclibrary/settings.py
+++ Music_Library_BE/musiclibrary/musiclibrary/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+CORS_ORIGIN_ALLOW_ALL=True 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'music',
+    'corsheaders', 
 ]
 
 MIDDLEWARE = [
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware’, 
 ]
 
 ROOT_URLCONF = 'musiclibrary.urls'
